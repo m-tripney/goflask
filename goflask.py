80118a958fbd77d48f726f143b0ef4aaadf46c6d
Mark Tripney <mark.tripney@gmail.com>
2020-08-24 15:36:10 +0200

    Add folder structure (if safe)

--- goflask.py
+++ goflask.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
-import glob, os, readline
-
+import glob
+import os
+import readline
+import venv
 
 def get_directory(text, state):
-    trailing = ""
     if "~" in text:
         text = os.path.expanduser("~")  # Go to user's home directory
     if os.path.isdir((glob.glob(text + "*"))[state]):
-        trailing = "/"  # Add a trailing slash if current location is a directory
-    return (glob.glob(text + "*"))[state] + trailing
+        return (glob.glob(text + "*"))[state] + "/"  # Add '/' if location is a dir.
+    return (glob.glob(text + "*"))[state]
+
+
+def make_dir(new_path, app_name):
+    os.chdir(new_path)
+    try:
+        os.mkdir(f"{app_name}_app")
+        os.chdir(f"{app_name}_app")
+        new_venv = venv.EnvBuilder()
+        new_file = open(f"{app_name}.py", "w")
+        os.mkdir("static")
+        os.mkdir("templates")
+    except FileExistsError:
+        print("A directory with that name already exists.")
+
+
+def create_venv(venv_name):
+    pass
 
 
 if __name__ == "__main__":
@@ -16,4 +34,12 @@ if __name__ == "__main__":
     readline.parse_and_bind("tab: complete")
     readline.set_completer(get_directory)
     print(f"\n\U0001F4CD PWD is {os.getcwd()}/")
-    input("\n\U0001F4C1 Please choose a root dir. for your project:  ")
+    path = input("\U0001F4C1 Root dir. for your project:  ")
+    project_name = input("\U0001F4DB Project name:  ")
+    confirm = input(
+        f"\U00002753 Create '{project_name}_app' in folder {path} (Y/N)?  "
+    ).lower()
+    if confirm == "y":
+        make_dir(path, project_name)
+    else:
+        print("Goodbye!")
